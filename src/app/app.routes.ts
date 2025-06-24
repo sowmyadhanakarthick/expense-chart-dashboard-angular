b04bc65dc8c408972095d66a6e9f2c93953275aa
sowmya MV <sowmyadhanakarthick@gmail.com>
2025-06-24 11:44:32 +0530

    Added lazy loading for expenses list

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,14 @@
 import { Routes } from '@angular/router';
 import { AddExpense } from './components/add-expense/add-expense';
-import { ExpenseList } from './components/expense-list/expense-list';
 
 export const routes: Routes = [
-  { path: '', component: AddExpense },
-  { path: 'list', component: ExpenseList },
+  { path: '', component: AddExpense, title: 'Add Expenses' },
+  {
+    path: 'list',
+    loadComponent: () =>
+      import('./components/expense-list/expense-list').then(
+        (m) => m.ExpenseList
+      ),
+    title: 'View Expenses',
+  },
 ];
